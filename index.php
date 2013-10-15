48c778a50f2a8dae2482bbb1a4183e4e095106c8
Aviles <Aviles@Aviles-PC>
2013-10-15 20:14:51 +0200

    la forks

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <p>Forks es una ciudad en el estado de Washington, EE. UU. La población era de 3.120 habitantes según el censo del 2007. Durante muchos años su economía se basó en la industria maderera. Con recientes declinaciones en la industria, Forks ha tenido que depender de correccionales cercanas, como Clallam Bay Corretional Center y Olympic Correction Center, como fuentes de trabajos. Forks es un destino popular para los pescadores deportivos que pescan salmón y trucha arco iris en los ríos cercanos. También lo es para los visitantes del Parque Nacional Olympic.</p>
+    </body>
+</html>
